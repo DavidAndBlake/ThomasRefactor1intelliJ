bf82b00224218379668456176512d9e3556c5d99
kcshyffer <league@WorthyMini-6.attlocal.net>
2019-03-14 15:29:02 -0700

    Got Thomas' animation speed fixed
    We synchronized thomas’s animation speed with the animationTicker. Now
    Thomas’s movement looks smoother.

--- src/com/wintrisstech/Control.java
+++ src/com/wintrisstech/Control.java
@@ -13,34 +13,33 @@ import static javax.imageio.ImageIO.read;
 /***********************************************************************************************
  * Main Contrtol Class
  ***********************************************************************************************/
-public class Control extends JComponent implements ActionListener, Runnable, KeyListener
-{
-   public Thomas thomas = new Thomas();
+public class Control extends JComponent implements ActionListener, Runnable, KeyListener {
+    public Thomas thomas = new Thomas();
 
     Ground ground = new Ground();
     Track track = new Track();
+    int thomasAnimationSpeed = 80;
     BackgroundObject backgroundObject = new BackgroundObject();
     Sky backgroundSky = new Sky();
     int thomasImageIndex = 0;
-    Timer ticker = new Timer(10,null);
-    public static void main(String[] args)
-    {
+    Timer thomasSpeedTicker = new Timer(50, this);
+    Timer animationTicker = new Timer(thomasAnimationSpeed,this);
+    boolean isThomasMovingLeft = false;
+    boolean isThomasMovingRight = false;
+
+    public static void main(String[] args) {
         System.out.println("ThomasFebruary" + "version 0.0, 1/25/19");
         SwingUtilities.invokeLater(new Control());
-
     }
-
     @Override
-    public void run()
-    {
+    public void run() {
         initializeGameWindow();
     }
 
     /***********************************************************************************************
      * Set up main JFrame
      ***********************************************************************************************/
-    private void initializeGameWindow()
-    {
+    private void initializeGameWindow() {
         JFrame gameWindow = new JFrame("SuperGame");
         gameWindow.setTitle("Thomas the tank");
         gameWindow.setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
@@ -49,81 +48,64 @@ public class Control extends JComponent implements ActionListener, Runnable, Key
         gameWindow.getContentPane().setBackground(new Color(200, 235, 255));
         gameWindow.addKeyListener(this);
         gameWindow.setVisible(true);
-
-        ticker.start();
-        {
-//            try
-//            {
-//                thomas.getThomasSpriteImageArray()[0] = read(getClass().getResource("Thomas1.png"));
-//                thomas.getThomasSpriteImageArray()[1] = read(getClass().getResource("Thomas2.png"));
-//                thomas.getThomasSpriteImageArray()[2] = read(getClass().getResource("Thomas3.png"));
-//                thomas.getThomasSpriteImageArray()[3] = read(getClass().getResource("Thomas4.png"));
-//                thomas.getThomasSpriteImageArray()[4] = read(getClass().getResource("Thomas5.png"));
-//                thomas.getThomasSpriteImageArray()[5] = read(getClass().getResource("Thomas6.png"));
-//                thomas.getThomasSpriteImageArray()[6] = read(getClass().getResource("Thomas7.png"));
-//                thomas.getThomasSpriteImageArray()[7] = read(getClass().getResource("Thomas8.png"));
-//                thomas.getReverseThomasImageArray()[0] = read(getClass().getResource("Reversed Thomas1.png"));
-//                thomas.getReverseThomasImageArray()[1] = read(getClass().getResource("Reversed Thomas2.png"));
-//                thomas.getReverseThomasImageArray()[2] = read(getClass().getResource("Reversed Thomas3.png"));
-//                thomas.getReverseThomasImageArray()[3] = read(getClass().getResource("Reversed Thomas4.png"));
-//                thomas.getReverseThomasImageArray()[4] = read(getClass().getResource("Reversed Thomas5.png"));
-//                thomas.getReverseThomasImageArray()[5] = read(getClass().getResource("Reversed Thomas6.png"));
-//                thomas.getReverseThomasImageArray()[6] = read(getClass().getResource("Reversed Thomas7.png"));
-//                thomas.getReverseThomasImageArray()[7] = read(getClass().getResource("Reversed Thomas8.png"));
-//            } catch (IOException e)
-//            {
-//                e.printStackTrace();
-//            }
-
-        }
-    }
-    public void drawThomas()
-    {
-     thomas.getThomasSpriteImageArray();
-     thomas.getReverseThomasImageArray();
+        animationTicker.start();
     }
 
-    public void paint(Graphics g)
-    {
-        if(thomasImageIndex > 7){
+    public void drawThomas() {
+        thomas.getThomasSpriteImageArray();
+        thomas.getReverseThomasImageArray();
+    }
+
+    public void paint(Graphics g) {
+        if (thomasImageIndex > 7) {
             thomasImageIndex = 0;
         }
-        Graphics2D g2 = (Graphics2D)g;
-        g2.fillOval(100,100,100,100);
-        g2.drawImage(thomas.getThomasSpriteImageArray()[thomasImageIndex], 500,500, this);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.fillOval(100, 100, 100, 100);
+        g2.drawImage(thomas.getThomasSpriteImageArray()[thomasImageIndex], 500, 500, this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
         repaint();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-//    if (ticker.){
-//
-//    }
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e)
-    {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e)
-    {
-        if(e.getKeyCode() == KeyEvent.VK_LEFT && ticker.isRepeats()){
+//        if(e.getSource() == (animationTicker)){
+//            thomasImageIndex++;
+//        }
+        if(isThomasMovingLeft == true){
             System.out.println(thomasImageIndex);
-    thomasImageIndex++;
+            thomasImageIndex++;
+        }
+        if(isThomasMovingRight == true){
+            System.out.println(thomasImageIndex);
+            thomasImageIndex++;
+        }
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
 
     }
 
     @Override
-    public void keyReleased(KeyEvent e)
-    {
-
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            isThomasMovingLeft = true;
+            isThomasMovingRight = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            isThomasMovingRight = true;
+            isThomasMovingLeft = false;
+        }
     }
 
-
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            isThomasMovingLeft = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            isThomasMovingLeft = false;
+        }
+    }
 }
 
--- src/com/wintrisstech/Thomas.java
+++ src/com/wintrisstech/Thomas.java
@@ -25,25 +25,25 @@ public class Thomas
     {
         try
         {
-            thomasSpriteImageArray[0] = ImageIO.read(new File("/Users/davidfrieder/git/ThomasRefactor1/src/Thomas1.png"));
-            thomasSpriteImageArray[1] = ImageIO.read(new File("/Users/davidfrieder/git/ThomasRefactor1/src/Thomas2.png"));
-            thomasSpriteImageArray[2] = ImageIO.read(new File("/Users/davidfrieder/git/ThomasRefactor1/src/Thomas3.png"));
-            thomasSpriteImageArray[3] = ImageIO.read(new File("/Users/davidfrieder/git/ThomasRefactor1/src/Thomas4.png"));
-            thomasSpriteImageArray[4] = ImageIO.read(new File("/Users/davidfrieder/git/ThomasRefactor1/src/Thomas5.png"));
-            thomasSpriteImageArray[5] = ImageIO.read(new File("/Users/davidfrieder/git/ThomasRefactor1/src/Thomas6.png"));
-            thomasSpriteImageArray[6] = ImageIO.read(new File("/Users/davidfrieder/git/ThomasRefactor1/src/Thomas7.png"));
-            thomasSpriteImageArray[7] = ImageIO.read(new File("/Users/davidfrieder/git/ThomasRefactor1/src/Thomas8.png"));
-            reverseThomasImageArray[0] = ImageIO.read(new File("ReversedThomas1.png"));
-            reverseThomasImageArray[1] = ImageIO.read(new File("ReversedThomas2.png"));
-            reverseThomasImageArray[2] = ImageIO.read(new File("ReversedThomas3.png"));
-            reverseThomasImageArray[3] = ImageIO.read(new File("ReversedThomas4.png"));
-            reverseThomasImageArray[4] = ImageIO.read(new File("ReversedThomas5.png"));
-            reverseThomasImageArray[5] = ImageIO.read(new File("ReversedThomas6.png"));
-            reverseThomasImageArray[6] = ImageIO.read(new File("ReversedThomas7.png"));
-            reverseThomasImageArray[7] = ImageIO.read(new File("ReversedThomas8.png"));
+            thomasSpriteImageArray[0] = ImageIO.read(new File("src/Thomas1.png"));
+            thomasSpriteImageArray[1] = ImageIO.read(new File("src/Thomas2.png"));
+            thomasSpriteImageArray[2] = ImageIO.read(new File("src/Thomas3.png"));
+            thomasSpriteImageArray[3] = ImageIO.read(new File("src/Thomas4.png"));
+            thomasSpriteImageArray[4] = ImageIO.read(new File("src/Thomas5.png"));
+            thomasSpriteImageArray[5] = ImageIO.read(new File("src/Thomas6.png"));
+            thomasSpriteImageArray[6] = ImageIO.read(new File("src/Thomas7.png"));
+            thomasSpriteImageArray[7] = ImageIO.read(new File("src/Thomas8.png"));
+            reverseThomasImageArray[0] = ImageIO.read(new File("src/ReversedThomas1.png"));
+            reverseThomasImageArray[1] = ImageIO.read(new File("src/ReversedThomas2.png"));
+            reverseThomasImageArray[2] = ImageIO.read(new File("src/ReversedThomas3.png"));
+            reverseThomasImageArray[3] = ImageIO.read(new File("src/ReversedThomas4.png"));
+            reverseThomasImageArray[4] = ImageIO.read(new File("src/ReversedThomas5.png"));
+            reverseThomasImageArray[5] = ImageIO.read(new File("src/ReversedThomas6.png"));
+            reverseThomasImageArray[6] = ImageIO.read(new File("src/ReversedThomas7.png"));
+            reverseThomasImageArray[7] = ImageIO.read(new File("src/ReversedThomas8.png"));
         } catch (IOException e)
         {
-            System.out.println("Ln 47 can't read the Thomas image from thomas constructor");
+            System.out.println("Ln 46 can't read the Thomas image from thomas constructor");
         }
     }
 
