c0e55ff22f6bf10cccf890207b68f1e0c53fe7b7
davidandblake <davidfrieder@gmail.com>
2019-06-07 16:56:34 -0700

    thomas forward & back
    Thomas moves forwards when his speed is greater than 0, and backwards when his speed is less than that. His speed is also adjustable.

--- src/com/wintrisstech/Control.java
+++ src/com/wintrisstech/Control.java
@@ -11,35 +11,18 @@ import static javax.imageio.ImageIO.read;
 
 /***********************************************************************************************
  * Main Contrtol Class
- * 5/5/19
+ * 6/5/19
  * Copyright David Frieder 2019
  * Changed the nature of thomas's speed control
  ***********************************************************************************************/
 
-//TODO if the left key is pressed, the speed will increase, and right will decrease
 public class Control extends JComponent implements ActionListener, Runnable, KeyListener
 {
     public Thomas thomas = new Thomas();
-    Ground ground = new Ground();
-    Track track = new Track();
-    BackgroundObject backgroundObject = new BackgroundObject();
-    Sky backgroundSky = new Sky();
-    int thomasImageIndex = 0;
-    double thomasSpeed = 7;
-    double thomasSpeedInverter = 50 - thomasSpeed;
+    int thomasSpeed = 7;
     Timer paintTicker = new Timer(20, this);
-    //    Timer thomasSpeedTicker = new Timer(thomasSpeed,this);
-    double tickerCounter;
-    double temp;
-    boolean isThomasMovingLeft = false;
-    boolean isThomasMovingRight = false;
-    boolean isThomasFacingLeft = true;
-    boolean isThomasFacingRight = false;
-    boolean isThomasJumping = false;
-    double wheelMovementTimeInterval = 0;
-    double wheelIncrementor = 50;
-    Image thomasFacingForward = thomas.getThomasSpriteImageArray()[thomasImageIndex];
-    Image thomasFacingReversed = thomas.getReverseThomasImageArray()[thomasImageIndex];
+    int pseudoThomasTicker;
+    int paintTickerCounter = 0;
 
     public static void main(String[] args)
     {
@@ -56,22 +39,19 @@ public class Control extends JComponent implements ActionListener, Runnable, Key
     /***********************************************************************************************
      * Set up main JFrame
      ***********************************************************************************************/
-
-
-    private void initializeGameWindow()
+    private void initializeGameWindow()    //Sets up the game window
     {
         JFrame gameWindow = new JFrame("Thomas the tank");
+        gameWindow.setVisible(true);
         gameWindow.setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
         gameWindow.add(this);// Adds the paint method to the JFrame
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.getContentPane().setBackground(new Color(200, 235, 255));
         gameWindow.addKeyListener(this);
-        gameWindow.setVisible(true);
         paintTicker.start();
-//        thomasSpeedTicker.start();
     }
 
-    public void drawThomas()
+    public void drawThomas()    //References the Thomas image arrays in the thomas class
     {
         thomas.getThomasSpriteImageArray();
         thomas.getReverseThomasImageArray();
@@ -80,51 +60,31 @@ public class Control extends JComponent implements ActionListener, Runnable, Key
     public void paint(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        if (isThomasMovingLeft)
-        {
-            g2.drawImage(thomas.getThomasSpriteImageArray()[thomasImageIndex], 500, 500, this);
-            g2.scale(.5, .5);
-            g2.drawImage(thomas.getArrowImages()[1], 700, 400, this);
-            thomasSpeed-=.5;
-//            if (thomasSpeed<2){
-//                thomasSpeed++;
-//            }
+
+        if(thomasSpeed > 0){
+            g2.drawImage(thomas.getThomasSpriteImageArray()[pseudoThomasTicker], 500, 500, 500, 300, this);
         }
-        if (isThomasFacingLeft)
-        {
-            g2.drawImage(thomas.getThomasSpriteImageArray()[thomasImageIndex], 500, 500, this);
-        }
-        if (isThomasMovingRight)
-        {
-            g2.drawImage(thomas.getReverseThomasImageArray()[thomasImageIndex], 500, 500, this);
-            g2.scale(.5, .5);
-            g2.drawImage(thomas.getArrowImages()[0], 700, 400, this);
-            thomasSpeed += .5;
-        }
-        if (isThomasFacingRight)
-        {
-            g2.drawImage(thomas.getReverseThomasImageArray()[thomasImageIndex], 500, 500, this);
+        else{
+            g2.drawImage(thomas.getReverseThomasImageArray()[pseudoThomasTicker], 500, 500, 500, 300, this);
         }
     }
-
+    //TODO: MAKE THOMAS MOVE LEFT WHEN THE SPEED IS NEGATIVE
     @Override
     public void actionPerformed(ActionEvent e)
     {
-
-        if (e.getSource() == paintTicker)
-        {
-            repaint();
-            tickerCounter++;
-           if(thomasSpeed > 0){
-               temp = tickerCounter % thomasSpeed;
-           }
-
-           if (tickerCounter % 2 == 0){
-               thomasImageIndex++;
-               thomasImageIndex %= 8;
-               System.out.println(thomasImageIndex);
-           }
+        paintTickerCounter++;
+        if(thomasSpeed != 0){
+            if (paintTickerCounter % (100 / thomasSpeed) == 0)
+            {
+                pseudoThomasTicker++;
+                pseudoThomasTicker %= 8;
+            }
         }
+        else{
+            pseudoThomasTicker = 0;
+        }
+        System.out.println(thomasSpeed);
+        repaint();
     }
 
     @Override
@@ -138,32 +98,21 @@ public class Control extends JComponent implements ActionListener, Runnable, Key
     {
         if (e.getKeyCode() == KeyEvent.VK_LEFT)
         {
-            isThomasMovingLeft = true;
-            isThomasMovingRight = false;
-
+            thomasSpeed++;
         }
         if (e.getKeyCode() == KeyEvent.VK_RIGHT)
         {
-            isThomasMovingRight = true;
-            isThomasMovingLeft = false;
+            thomasSpeed--;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+        {
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e)
     {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT)
-        {
-            isThomasMovingLeft = false;
-            isThomasFacingLeft = true;
-            isThomasFacingRight = false;
-        }
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-        {
-            isThomasMovingRight = false;
-            isThomasFacingRight = true;
-            isThomasFacingLeft = false;
-        }
+
     }
 }
 
