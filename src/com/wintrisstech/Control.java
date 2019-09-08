22cfa061ae2adcdcd2c3c17e049313a43af078d0
davidandblake <davidfrieder@gmail.com>
2019-09-08 12:42:52 -0700

    Checking to see if this can be uploaded

--- src/com/wintrisstech/Control.java
+++ src/com/wintrisstech/Control.java
@@ -19,13 +19,15 @@ import static javax.imageio.ImageIO.read;
 public class Control extends JComponent implements ActionListener, Runnable, KeyListener
 {
     public Thomas thomas = new Thomas();
+    public GamePhysics physics = new GamePhysics();
     int thomasSpeed = 7;
     int acceleration;
     Timer paintTicker = new Timer(20, this);
-    int thomasSpeedTicker;
+    int thomasFrameNumber;
     int accelerationTicker;
     int paintTickerCounter = 0;
     Image lastImage;
+
 
     public static void main(String[] args)
     {
@@ -46,7 +48,7 @@ public class Control extends JComponent implements ActionListener, Runnable, Key
     {
         JFrame gameWindow = new JFrame("Thomas the tank");
         gameWindow.setVisible(true);
-        gameWindow.setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
+        gameWindow.setSize( Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
         gameWindow.add(this);// Adds the paint method to the JFrame
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.getContentPane().setBackground(new Color(200, 235, 255));
@@ -66,13 +68,13 @@ public class Control extends JComponent implements ActionListener, Runnable, Key
         Graphics2D g2 = (Graphics2D) g;
         if(thomasSpeed > 0)
         {
-            g2.drawImage(thomas.getThomasSpriteImageArray()[thomasSpeedTicker], 500, 500, 500, 300, this); //this makes thomas face right
-            lastImage = thomas.getThomasSpriteImageArray()[thomasSpeedTicker];
+            g2.drawImage(thomas.getThomasSpriteImageArray()[thomasFrameNumber], 500, 500, 500, 300, this); //this makes thomas face right
+            lastImage = thomas.getThomasSpriteImageArray()[thomasFrameNumber];
         }
         if(thomasSpeed < 0)
         {
-            g2.drawImage(thomas.getReverseThomasImageArray()[thomasSpeedTicker], 500, 500, 500, 300, this); //this makes thomas face left
-            lastImage = thomas.getReverseThomasImageArray()[thomasSpeedTicker];
+            g2.drawImage(thomas.getReverseThomasImageArray()[thomasFrameNumber], 500, 500, 500, 300, this); //this makes thomas face left
+            lastImage = thomas.getReverseThomasImageArray()[thomasFrameNumber];
         }
         if(thomasSpeed == 0)
         {
@@ -88,14 +90,14 @@ public class Control extends JComponent implements ActionListener, Runnable, Key
         {
             if (paintTickerCounter % (100 / thomasSpeed) == 0)
             {
-                thomasSpeedTicker++;
-                thomasSpeedTicker %= 8;
+                thomasFrameNumber++;
+                thomasFrameNumber %= 8;
             }
         } else
         {
-            thomasSpeedTicker = 0;
+            thomasFrameNumber = 0;
         }
-        System.out.println("thomasSpeed = " + thomasSpeed  + " thomasSpeedTicker = " + thomasSpeedTicker);
+        System.out.println("thomasSpeed = " + thomasSpeed  + " thomasFrameNumber = " + thomasFrameNumber);
         repaint();
     }
     public void setAcceleration(boolean isSpeedDecreasing, boolean isSpeedIncreasing)
--- src/com/wintrisstech/GamePhysics.java
+++ src/com/wintrisstech/GamePhysics.java
@@ -1,11 +1,62 @@
 package com.wintrisstech;
 
-import java.util.Timer;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.Timer;
 
-public class GamePhysics
+public class GamePhysics implements ActionListener
 {
-    //Timer // One second. Let me get a working version of this program running first
-    void ThomasAcceleration(){
+    Timer accelerationTicker = new Timer(50, this); //This will control the rate of change in Thomas' acceleration
+    boolean isGoingLeft;                                         //This is a copy of the boolean in the Control class
+    boolean isGoingRight;                                        //This is a copy of the boolean in the Control class
+    boolean isAcceleratingDueToGravity;
+    int thomasSpeed = 0;                                         //An integer value that sets Thomas' speed to 0
+    int thomasStaringVerticalSpeed = 0;
+    int thomasCurrentVerticalSpeed = thomasStaringVerticalSpeed;
+    int gravityAcceleration = 1;
+    int thomasJumpingSpeed = 8;
 
+    GamePhysics()
+    {                  //This is the constructor
+        accelerationTicker.start(); //This starts the acceleration Ticker
+    }
+
+    void ThomasAcceleration(ActionEvent e)
+    {
+        if (isGoingLeft)
+        {              //Boolean that checks whether Thomas is moving left
+            thomasSpeed--;            //Change in Thomas' speed
+        }
+        if (isGoingRight)
+        {             //Boolean that checks if Thomas is moving right
+            thomasSpeed++;            //Changes Thomas' speed
+        }
+    }
+
+    public void getThomasSpeed()
+    {
+        int thomasSpeed = this.thomasSpeed;
+    }
+
+    void GravityAcceleration()
+    {
+        if (isAcceleratingDueToGravity)
+        {
+            thomasCurrentVerticalSpeed += gravityAcceleration;
+        }
+    }
+
+    void getThomasVerticalSpeed()
+    {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        ThomasAcceleration(e);
     }
 }
